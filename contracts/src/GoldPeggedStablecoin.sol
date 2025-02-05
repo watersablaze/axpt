9f304d257f64a53b5d80dbf1fe4724daef8b3d9a
Waters Ablaze <mayasvisions@gmail.com>
2025-02-05 21:41:12 +0200

    removed chainlink-contracts

--- contracts/src/GoldPeggedStablecoin.sol
+++ contracts/src/GoldPeggedStablecoin.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "chainlink-contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract GoldPeggedStablecoin is ERC20, Ownable {
     AggregatorV3Interface internal goldPriceFeed;
