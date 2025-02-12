3ddcb1c1ade77c09213b62d30226d85e8cbe5414
Waters Ablaze <mayasvisions@gmail.com>
2025-02-12 20:13:50 +0200

    Running Tests on deployed contracts

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -2,15 +2,17 @@
 pragma solidity ^0.8.19;
 
 import "forge-std/Script.sol";
-import "../src/GoldPeggedStablecoin.sol";
+import "../src/GoldPeggedStablecoin.sol"; // Ensure this path is correct!
 
-contract DeployGoldStablecoin is Script {
+contract Deploy is Script {
     function run() external {
         vm.startBroadcast();
 
-        // Replace with your Chainlink Gold Price Feed address
-        address goldPriceFeed = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
-        new GoldPeggedStablecoin(goldPriceFeed);
+        // Option 1: Hardcoded Address (Make sure it's correct!)
+        new GoldPeggedStablecoin(0x694AA1769357215DE4FAC081bf1f309aDC325306); 
+        
+        // Option 2: Use ENV Variable (Preferred for flexibility)
+        // new GoldPeggedStablecoin(vm.envAddress("CHAINLINK_PRICE_FEED")); 
 
         vm.stopBroadcast();
     }
