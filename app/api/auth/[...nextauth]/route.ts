cc84463e83ce270e9adc568e464687dd2fda2bf8
Waters Ablaze <170494925+watersablaze@users.noreply.github.com>
2025-04-29 18:24:02 +0200

    🔥 Remove broken nextauth route for clean deploy

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,8 +0,0 @@
-import NextAuth from "next-auth";
-import authOptions from "@/app/api/auth/authOptions"; // ✅ Correctly import `authOptions`
-
-// ✅ Create NextAuth handler
-const handler = NextAuth(authOptions);
-
-// ✅ Export handler correctly
-export { handler as GET, handler as POST };
