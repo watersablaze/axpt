1a57efc40b36df45734d0c75fe1a2e8cd883cb9c
Waters Ablaze <170494925+watersablaze@users.noreply.github.com>
2025-12-06 22:46:51 +0200

    Remove uppercase Live ghost folder

--- src/components/Live/StreamKeyPanel.tsx
+++
@@ -1,7 +0,0 @@
-export default function StreamKeyPanel() {
-  return (
-    <div className="text-gray-400 p-4 border border-gray-700 rounded-lg">
-      ComponentName placeholder
-    </div>
-  );
-}
